fb0f4f908f6b2b7d5235bb17520c210ca6e34b21
wangyebin <1451782140@qq.com>
2020-11-02 23:46:35 +0800

    新增了几个文件

---
+++ src/change/Mychange.java
@@ -0,0 +1,7 @@
+package change;
+
+public class Mychange {
+    public static void main(String[] args) {
+        System.out.println("finish");
+    }
+}
---
+++ src/finish/Myhappy.java
@@ -0,0 +1,7 @@
+package finish;
+
+public class Myhappy {
+    public static void main(String[] args) {
+        System.out.println("hahah");
+    }
+}
